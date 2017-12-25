8e930260093b2b97c69ee6002ae4991ea4a687ae
Koen van Vliet <8by8mail@gmail.com>
2017-12-25 22:57:57 +0100

    Basic collision detection between player and enemy actors

--- sprites.py
+++ sprites.py
@@ -1,32 +1,68 @@
 import pygame
 from pygame.locals import *
 
+# TODO do not use constants for frame-rate
+
 img_ball = pygame.image.load("assets/ball.bmp")
+img_ball_inv = pygame.image.load("assets/ball_inv.bmp")
 
 class Actor(pygame.sprite.Sprite):
+	"""	Base class for all objects that can move, interact with terrain and eachother """
 
 	def __init__(self, x, y):
-		# Call base class constructor
+		# Call base class (Sprite) constructor
 		pygame.sprite.Sprite.__init__(self)
-
+		self.image = self.image_normal
 		self.rect = self.image.get_rect().move(x, y)
 		self.xvel, self.yvel = 0, 0
+		self.inv_frames = 0
 
-	def update(self, xvel = self.xvel, yvel = self.yvel):
+
+	def update(self, xvel = None, yvel = None):
+		# Update velocity
+		if xvel is not None:
+			self.xvel = xvel
+		if yvel is not None:
+			self.yvel = yvel
+		# Update position based on velocity
 		self.rect = self.rect.move(self.xvel, self.yvel)
-		pass
+
+		# Count down invincibility frame counter
+		if self.inv_frames:
+			if self.inv_frames % 8 > 4:
+				self.image = self.image_inv
+			else:
+				self.image = self.image_normal
+
+			self.inv_frames -= 1
 
 class Player(Actor):
+	max_inv_frames = 60
+
 	def __init__(self, x, y):
-		self.image = img_ball
-		# Call base class constructor
+		self.image_normal = img_ball
+		self.image_inv = img_ball_inv
+		# Call base class (Actor) constructor
 		Actor.__init__(self, x, y)
 
+	def hit(self):
+		self.inv_frames = self.max_inv_frames
+
 class Enemy(Actor):
+	max_inv_frames = 15
+
 	def __init__(self, x, y):
-		self.image = img_ball
-		# Call base class constructor
+		self.image_normal = img_ball
+		self.image_inv = img_ball_inv
+		# Call base class (Actor) constructor
 		Actor.__init__(self, x, y)
+
+	def hit(self):
+		self.inv_frames = self.max_inv_frames
+
+	def update(self, xvel = None, yvel = None):
+		# Call base class (Actor) update method
+		Actor.update(self, xvel = xvel, yvel = yvel)
 
 class Controller:
 	""" TODO create virtual joystick base class"""
@@ -84,6 +120,9 @@ class App:
 		# Create a virtual joystick object
 		self.joy = Controller()
 
+		# Create a clock for limiting the framerate
+		self.clk = pygame.time.Clock()
+
 	def on_event(self, event):
 		if event.type == pygame.QUIT:
 			self._running = False
@@ -91,15 +130,21 @@ class App:
 			self.joy.update(event.key, event.type)
 
 	def on_loop(self):
-		x,y = joy.get_xy()
+		col = pygame.sprite.groupcollide(self.g_player, self.g_enemies, False, False)
+
+		for spr1,spr2 in col.iteritems():
+			spr1.hit()
+
+		x,y = self.joy.get_xy()
 		self.g_player.sprites()[0].update(x,y)
 		self.g_enemies.update()
 
 	def on_render(self):
+		self._display_surf.fill(pygame.Color(0,0,0))
 		self.g_player.draw(self._display_surf)
 		self.g_enemies.draw(self._display_surf)
 		pygame.display.flip()
-		pass
+		self.clk.tick(60)
 
 	def on_cleanup(self):
 		pygame.quit()
